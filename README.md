50ab6d973688689c42aae604980f4325e6e7f158
Vivek Trivedi <32205003+trivik19780@users.noreply.github.com>
2024-11-07 15:14:43 +0530

    firstcommit
    init repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# demo-sentinel
+# demo-sentinel
---
+++ repo.sh
@@ -0,0 +1,7 @@
+echo "# demo-sentinel" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/trivik2024/demo-sentinel.git
+git push -u origin main
